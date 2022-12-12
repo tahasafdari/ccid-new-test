0635a7be3d49acad5546d396ac12b428df4ab935
tahasafdari <41860385+tahasafdari@users.noreply.github.com>
2022-12-12 10:25:38 +0200

    app js modified

--- src/App.js
+++ src/App.js
@@ -1,54 +1,59 @@
-import React from 'react'
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
+import "./App.css";
 
 export const dataReducer = (state, action) => {
-  if (action.type === 'SET_ERROR') {
-    return { ...state, list: [], error: true }
+  if (action.type === "SET_ERROR") {
+    return { ...state, list: [], error: true };
   }
 
-  if (action.type === 'SET_LIST') {
-    return { ...state, list: action.list, error: null }
+  if (action.type === "SET_LIST") {
+    return { ...state, list: action.list, error: null };
   }
 
-  throw new Error()
-}
+  throw new Error();
+};
 
 const initialData = {
   list: [],
   error: null,
-}
+};
 
 const App = () => {
-  const [counter, setCounter] = React.useState(0)
-  const [data, dispatch] = React.useReducer(dataReducer, initialData)
+  const [counter, setCounter] = React.useState(0);
+  const [data, dispatch] = React.useReducer(dataReducer, initialData);
 
   React.useEffect(() => {
     axios
-      .get('http://hn.algolia.com/api/v1/search?query=react')
+      .get("http://hn.algolia.com/api/v1/search?query=react")
       .then((response) => {
-        dispatch({ type: 'SET_LIST', list: response.data.hits })
+        dispatch({ type: "SET_LIST", list: response.data.hits });
       })
       .catch(() => {
-        dispatch({ type: 'SET_ERROR' })
-      })
-  }, [])
+        dispatch({ type: "SET_ERROR" });
+      });
+  }, []);
 
   return (
-    <div>
-      <h1>My Counter</h1>
-      <Counter counter={counter} />
+    <div id="root">
+      <div id="counter-block">
+        <h1>My Counter</h1>
+        <Counter counter={counter} />
 
-      <button type='button' onClick={() => setCounter(counter + 1)}>
-        Increment
-      </button>
+        <div id="button-box">
+          <button type="button" onClick={() => setCounter(counter + 1)}>
+            Increment
+          </button>
 
-      <button type='button' onClick={() => setCounter(counter - 1)}>
-        Decrement
-      </button>
+          <button type="button" onClick={() => setCounter(counter - 1)}>
+            Decrement
+          </button>
+        </div>
+      </div>
 
       <h2>My Async Data</h2>
 
-      {data.error && <div className='error'>Error</div>}
+      {data.error && <div className="error">Error</div>}
 
       <ul>
         {data.list.map((item) => (
@@ -56,13 +61,13 @@ const App = () => {
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
 export const Counter = ({ counter }) => (
-  <div>
+  <div id="counter">
     <p>{counter}</p>
   </div>
-)
+);
 
-export default App
+export default App;
